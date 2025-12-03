54f7d8488422fc79ccc1b8d54427046fbc14f909
feserm <feser@ipk-gatersleben.de>
2025-12-03 14:43:51 +0000

    Debug Console can be turned off using general config env != 'dev'

--- src/routes/submit/+page.svelte
+++ src/routes/submit/+page.svelte
@@ -3,12 +3,15 @@
 	import Console from "$lib/components/submission/Console.svelte";
 	import Questionnaire from "$lib/components/submission/Questionnaire.svelte";
 	import { datasetObj } from "$lib/stores/dataset";
+	import generalConfig from "$lib/config/general.json";
 
     $datasetObj = Schemas.getObjectFromSchema('dataset');
 
 </script>
 
 <section class="w-2/3 mx-auto">
+	{#if generalConfig.env === 'dev'}
     <Console/>
+	{/if}
     <Questionnaire/>
 </section>
